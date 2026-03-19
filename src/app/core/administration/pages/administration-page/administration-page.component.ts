2d3f78f0927119b839409c99d25c12e41ff89134
Vadym Bei <bey1705@gmail.com>
2026-03-19 14:01:56 +0200

    Update styles for administration page

--- src/app/core/administration/pages/administration-page/administration-page.component.ts
+++ src/app/core/administration/pages/administration-page/administration-page.component.ts
@@ -1,50 +1,14 @@
-import { Component } from '@angular/core';
-import { AdministrationItem } from '../../models/administration-item.model';
-import { RouterLink
+import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
- } from '@angular/router';
-import {NgClass} from "@angular/common";
 @Component({
   selector: 'app-administration-page',
   standalone: true,
   imports: [
     RouterLink,
-    NgClass
   ],
   templateUrl: './administration-page.component.html',
   styleUrl: './administration-page.component.scss'
 })
 export class AdministrationPageComponent {
-  administrations: AdministrationItem[] = [
-    {
-      title: 'Регіони',
-      path: '/administration/units',
-      icon: 'bi bi-pin-map',
-    },
-    {
-      title: 'Департаменти',
-      path: '/administration/departments',
-      icon: 'bi bi-shop',
-    },
-    {
-      title: 'Посади',
-      path: '/administration/positions',
-      icon: 'bi bi-person-workspace',
-    },
-    {
-      title: 'Працівники',
-      path: '/administration/employees',
-      icon: 'bi bi-people'
-    },
-    {
-      title: 'Робочі зміни',
-      path: '/administration/shifts',
-      icon: 'bi bi-calendar-week',
-    },
-    {
-      title: 'Ролі працівників',
-      path: '/administration/employees-roles',
-      icon: 'bi bi-person-rolodex',
-    }
-  ]
 }
